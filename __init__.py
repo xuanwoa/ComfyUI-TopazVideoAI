feff619fb273172e15e02ba6420a7601feb63346
Raven <31123567+sh570655308@users.noreply.github.com>
2025-01-13 17:30:02 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
-from .topaz_video_node import TopazVideoAINode
+from .topaz_video_node import TopazVideoAINode,TopazUpscaleParamsNode
 
 # 定义节点类映射
 NODE_CLASS_MAPPINGS = {
-    "TopazVideoAI": TopazVideoAINode
+    "TopazVideoAI": TopazVideoAINode,
+    "TopazUpscaleParams": TopazUpscaleParamsNode
 }
 
-# 定义节点显示名称映射
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "TopazVideoAI": "Topaz Video AI (Upscale & Frame Interpolation)"
+    "TopazVideoAI": "Topaz Video AI (Upscale & Frame Interpolation)",
+    "TopazUpscaleParams": "Topaz Upscale Parameters"
 }
